2c7c665137019477de81b4d46bd8d11f5ebdbe9c
jwoo9 <jwyutong@gmail.com>
2024-12-10 15:06:22 +0000

    ready for grading - 12/10/24

---
+++ StackQueueMini.java
@@ -0,0 +1,16 @@
+//Justin Wu, 12/9/24, PD 3
+//Description: 
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+class StackQueueMini {
+  public static void main(String[] args) {
+
+    for (int i = 0; i<5; i++){
+      System.out.println("Enter a whole number");
+
+    }
+  } // end main
+} // end class
